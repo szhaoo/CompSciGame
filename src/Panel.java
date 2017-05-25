c38033674a89e7737462e1abb1ae7dd6106cdabb
szhaoo <serena.zhao.1025@gmail.com>
2017-05-25 13:38:09 -0400

    Automated

--- src/Panel.java
+++ src/Panel.java
@@ -1,14 +1,20 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
+import javax.swing.Timer;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class Panel extends JPanel
+public class Panel extends JPanel implements ActionListener, KeyListener
 {
+  int x = 350;
+  private static final int Y = 500;
   BufferedImage img;
   public Panel(){
     setBackground(new Color(25, 170, 200));
@@ -18,12 +24,29 @@ public class Panel extends JPanel
       System.out.println("Bad Image");
       System.exit(1);
     }
+    addKeyListener(this);
+    Timer timer = new Timer(1000/60, this);
+    timer.start();
+    requestFocus(); //penis
   }
   
   
   public void paintComponent(Graphics g){
     super.paintComponent(g);
-    g.drawImage(img, 350, 500, this); // Ha 420
+    g.drawImage(img, x, Y, this); // Ha 420
   }
-
+  
+  public void actionPerformed(ActionEvent e){
+    step();
+  }
+  public void step(){
+    repaint();
+  }
+  public void keyTyped(KeyEvent e) {
+  }
+  public void keyPressed(KeyEvent e) {
+    System.out.println(e.getKeyCode());
+  }
+  public void keyReleased(KeyEvent e) {
+  }
 }
